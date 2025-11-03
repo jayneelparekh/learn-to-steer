a888321e376efe7250413f52683e344f44c90615
Jayneel Parekh <jayneelparekh@gmail.com>
2025-11-03 01:37:03 +0100

    Update links + teaser figure

--- src/examples/learned_steering/L2S/safety/3_response_eval.sh
+++ src/examples/learned_steering/L2S/safety/3_response_eval.sh
@@ -12,7 +12,7 @@ analysis_name="safety_metrics"
 #response_filename="llava_vlguard_response_test_2.0_l2s.pth"
 response_filename="llava_mmsb_test_steer_15_multi_2.2_steering_net_v3_multi_nobias_K100.pth"
 #response_filename="llava_mmsb_test_steer_default_multi_2.2_llava_mmsb_steering_net_v3_multi_nobias_K100.pth"
-#response_filename="qwen_mmsb_test_steer__multi_2.2_qwen_14_last_input_multi_mmsb_train_-1_v2.pth"
+#response_filename="qwen_mmsb_test_steer_l2s_multi_2.2_qwen_14_last_input_multi_mmsb_train_-1_v2.pth"
 
 
 python src/analyse_features.py \
